bceccc0336e9394580a95fa6cbca68de65d80ff0
prudhvirajkrishna <prudhviraj123k@gmail.com>
2023-01-12 09:39:05 +0530

    initialise project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './logo.svg';
+import "./App.css";
+import Weather from "./weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+      
+    </div>
+  );
+}
+
+export default App
